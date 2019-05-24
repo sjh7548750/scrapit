761d7f66664bbcbc97c646f27e0ca64b035eea78
junyeon <wnsdus306@likelion.org>
2019-05-25 02:02:11 +0900

    0202 JOON

--- scrap/admin.py
+++ scrap/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Scrap
+from .models import Folder, Scrap
+admin.site.register(Folder)
 admin.site.register(Scrap)
-
 
 # Register your models here.
 
